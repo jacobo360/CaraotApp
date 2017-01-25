e3f0c26698076daf9b6f92a75ab8a9092e00e812
Jacobo Koenig <jacobo360@gmail.com>
2017-01-25 11:51:36 -0500

    Committing

--- CaraotApp/SearchViewController.swift
+++ CaraotApp/SearchViewController.swift
@@ -22,7 +22,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
         searchBar.delegate = self
         tblView.delegate = self
         tblView.dataSource = self
